14f700434f9d213905a5629086fa20a93b4baa67
Konstantin Serditov <serditov@gmail.com>
2014-08-04 18:56:31 +0400

    KSerditov. Adjust variable names for android naming conventions. Replace == to .equals. Save chosen folder result with SharedProperties.

--- src/ru/kserditov/simplemp3player/FolderChooser.java
+++ src/ru/kserditov/simplemp3player/FolderChooser.java
@@ -2,6 +2,7 @@ package ru.kserditov.simplemp3player;
 
 import java.io.File;
 import java.io.FilenameFilter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -9,9 +10,13 @@ import java.util.Comparator;
 import java.util.List;
 
 import android.app.Activity;
+
 import android.content.SharedPreferences;
+
 import android.os.Bundle;
+
 import android.view.View;
+
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -21,37 +26,35 @@ import android.widget.Toast;
 
 public class FolderChooser extends Activity {
 
-	ListView listView = null;
-	List<File> fileList = null;
-	File parentDir = new File("..");
-	Toast toast = null;
-	TextView tvSelectedFolder = null;
-	CustomOnItemClickListener listener = null;
-	SharedPreferences settings = null;
-	SharedPreferences.Editor editor = null;
+	private ListView mListView = null;
+	private List<File> mFileList = null;
+	private static final File sParentDir = new File("..");
+	private Toast mToast = null;
+	private TextView mTvSelectedFolder = null;
+	private CustomOnItemClickListener mListener = null;
+	private SharedPreferences mSharedPreferences = null;
+	private SharedPreferences.Editor mSharedPreferencesEditor = null;
 
 	// Comparator for sorting File objects
-	Comparator<File> fileComparator = new Comparator<File>() {
+	private Comparator<File> mFileComparator = new Comparator<File>() {
 		public int compare(File f1, File f2) {
 
 			String s1 = f1.getName();
 			String s2 = f2.getName();
 
 			// Make sure Up is always on top of the list
-			if (s1 == "..") {
+			if ("..".equals(s1)) {
 				return -1;
 			}
-
-			if (s2 == "..") {
+			if ("..".equals(s2)) {
 				return 1;
 			}
-
 			return s1.compareToIgnoreCase(s2);
 		}
 	};
 
 	// Filtering folders accessible for user
-	FilenameFilter directoryFilter = new FilenameFilter() {
+	private FilenameFilter mDirectoryFilter = new FilenameFilter() {
 		@Override
 		public boolean accept(File dir, String filename) {
 			File f = new File(dir, filename);
@@ -65,20 +68,21 @@ public class FolderChooser extends Activity {
 
 		// Initialize activity UI elements
 		setContentView(R.layout.activity_folder_chooser);
-		listView = (ListView) findViewById(R.id.lstFolders);
-		tvSelectedFolder = (TextView) findViewById(R.id.txtSelectedFolder);
+		mListView = (ListView) findViewById(R.id.lstFolders);
+		mTvSelectedFolder = (TextView) findViewById(R.id.txtSelectedFolder);
 
 		// Read previously selected folder and try to open it
-		settings = getPreferences(MODE_PRIVATE);
+		mSharedPreferences = getPreferences(MODE_PRIVATE);
 
 		// Open root folder if there is not preserved setting yet
-		File currentFolder = new File(settings.getString("folder", "/"));
+		File mCurrentFolder = new File(mSharedPreferences.getString("folder",
+				"/"));
 
 		// Open root folder if previously selected folder doesn't exist anymore
 		// or is not accessible anymore
-		if (currentFolder.exists() && currentFolder.canRead()
-				&& !currentFolder.isHidden()) {
-			updateList(currentFolder);
+		if (mCurrentFolder.exists() && mCurrentFolder.canRead()
+				&& !mCurrentFolder.isHidden()) {
+			updateList(mCurrentFolder);
 		} else {
 			updateList(new File("/"));
 		}
@@ -88,39 +92,40 @@ public class FolderChooser extends Activity {
 	private void updateList(File path) {
 
 		try {
-			fileList = new ArrayList<File>(Arrays.asList(path
-					.listFiles(directoryFilter)));
+			mFileList = new ArrayList<File>(Arrays.asList(path
+					.listFiles(mDirectoryFilter)));
 
 			// Adding Up element to the list for every folder other than root
 			if (path.getParent() != null) {
-				fileList.add(parentDir);
+				mFileList.add(sParentDir);
 			}
 
 			// Sorting elements by names
-			Collections.sort(fileList, fileComparator);
+			Collections.sort(mFileList, mFileComparator);
 
 			ArrayAdapter<File> adapter = new ArrayAdapter<File>(this,
-					android.R.layout.simple_list_item_1, fileList);
-			listView.setAdapter(adapter);
-			listener = new CustomOnItemClickListener(path);
-			listView.setOnItemClickListener(listener);
+					android.R.layout.simple_list_item_1, mFileList);
+			mListView.setAdapter(adapter);
+			mListener = new CustomOnItemClickListener(path);
+			mListView.setOnItemClickListener(mListener);
 
 			// Updating UI with currently opened folder
-			tvSelectedFolder.setText(path.getPath());
+			mTvSelectedFolder.setText(path.getPath());
 
 		} catch (NullPointerException e) {
-			// Showing popup message if can't go down to selected folder
-			toast = Toast.makeText(getApplicationContext(), getResources()
+			// Showing pop-up message if can't go down to selected folder
+			mToast = Toast.makeText(getApplicationContext(), getResources()
 					.getString(R.string.no_folder_access), Toast.LENGTH_SHORT);
-			toast.show();
+			mToast.show();
 		}
 	}
 
 	// Preserving selected folder in SharedPreferences
 	public void confirmFolder(View view) {
-		editor = settings.edit();
-		editor.putString("folder", tvSelectedFolder.getText().toString());
-		editor.commit();
+		mSharedPreferencesEditor = mSharedPreferences.edit();
+		mSharedPreferencesEditor.putString("folder", mTvSelectedFolder
+				.getText().toString());
+		mSharedPreferencesEditor.commit();
 		finish();
 	}
 
@@ -128,18 +133,18 @@ public class FolderChooser extends Activity {
 	// folder
 	private class CustomOnItemClickListener implements OnItemClickListener {
 
-		File path = new File("/");
+		File mPath = new File("/");
 
 		CustomOnItemClickListener(File _path) {
 			super();
-			path = _path;
+			mPath = _path;
 		}
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
-			if (parent.getAdapter().getItem(position).toString() == "..") {
-				updateList(new File(path.getParent()));
+			if ("..".equals(parent.getAdapter().getItem(position).toString())) {
+				updateList(new File(mPath.getParent()));
 			} else {
 				updateList((File) parent.getAdapter().getItem(position));
 			}
